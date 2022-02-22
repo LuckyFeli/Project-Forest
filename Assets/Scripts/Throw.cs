4a86491c2fe5af923e610db4915fe93f4f5ed1a1
RealLuckyToast <felix.haensel@stud.hs-kempten.de>
2022-02-22 13:50:33 +0100

    added cd
    cooldown for throw

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -9,7 +9,8 @@ public class Throw : MonoBehaviour
     private GameObject SCopy;
     private Rigidbody SCopyRB;
     private Collider SCopyCol;
-
+    private bool cooldown = true;
+    public float cd = 1f;
     // Start is called before the first frame update
     public void Start()
     {
@@ -19,7 +20,7 @@ public class Throw : MonoBehaviour
     public void ThrowObject(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
 
-        if (context.performed)
+        if (context.performed && cooldown)
         {
             Vector3 prePos = Selection.transform.position;
             SCopy = Instantiate(Selection);
@@ -32,10 +33,16 @@ public class Throw : MonoBehaviour
             SCopyRB.drag = 3f;
             SCopyRB.AddForce(Camera.main.transform.forward * 250);
             Debug.Log("Throw!" + prePos);
+            cooldown = false;
+            StartCoroutine(Cooldown());
         }
         
     }
-
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cd);
+        cooldown = true;
+    }
     // Update is called once per frame
     public void Update()
     {
