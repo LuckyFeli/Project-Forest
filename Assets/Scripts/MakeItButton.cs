42a9c3c24fde2cd56211185d598fa758c47ee3c7
Marcel Sturm <marcel.sturm2001@gmail.com>
2022-02-24 15:32:53 +0100

    outline

--- Assets/Scripts/MakeItButton.cs
+++ Assets/Scripts/MakeItButton.cs
@@ -5,12 +5,14 @@ using UnityEngine.Events;
 
 public class MakeItButton : MonoBehaviour
 {
+    private Outline outline;
     public UnityEvent unityEvent = new UnityEvent();
     public GameObject button;
     // Start is called before the first frame update
     void Start()
     {
         button = this.gameObject;
+        outline = GetComponent<Outline>();
     }
 
     // Update is called once per frame
@@ -18,12 +20,18 @@ public class MakeItButton : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Input.GetMouseButtonDown(0))
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
         {
-            if(Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+            outline.enabled = true;
+            if(Input.GetKeyDown(KeyCode.E))
             {
                 unityEvent.Invoke();
             }
         }
+        else
+        {
+            outline.enabled = false;    
+        }
+       
     }
 }
