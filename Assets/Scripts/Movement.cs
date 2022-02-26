563956c504d85447615ea8830aa64a30388546b6
RealLuckyToast <felix.haensel@stud.hs-kempten.de>
2022-02-26 09:34:36 +0100

    added code logic for teleport

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -43,7 +43,7 @@ public class Movement : MonoBehaviour
     private void Awake()
     {
         controls = new PlayerControls();
-        this.transform.position = pauseManager.instance.loadGame();
+        
 
     }
 
